cbcd5abe4d317fae81dae776db096e4addb7cef1
vigneswariCoder <vigneswaris2002@gmail.com>
2025-01-04 00:14:07 -0500

    updates in db and routes

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,8 +1,6 @@
-// db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
-// MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
@@ -13,16 +11,11 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
