69b683c2290ba41f6adf32a6396bb264fc6c81e8
vigneswariCoder <vigneswaris2002@gmail.com>
2025-01-04 04:03:31 -0500

    updates in login page

--- giftlink-frontend/src/components/LoginPage.css/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage.css/LoginPage.js
@@ -1,14 +1,55 @@
 import React, { useState,useEffect } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 import './LoginPage.css';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
 
     const handleLogin = async (e) => {
         e.preventDefault();
-}
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
 
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+          setIsLoggedIn(true);
+          navigate('/app');
+        } else {
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+      }
 
     return (
         <div className="container mt-5">
@@ -24,7 +65,7 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
                             />
                         </div>
                         <div className="mb-4">
@@ -35,8 +76,10 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
                             />
+
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                         </div>
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                         <p className="mt-4 text-center">
