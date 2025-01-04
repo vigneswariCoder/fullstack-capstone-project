b882c5031df3cb6473fcabf6d4a4d769858a2c20
vigneswariCoder <vigneswaris2002@gmail.com>
2025-01-04 03:58:36 -0500

    updates in navbar

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,80 +1,95 @@
-import React, { useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { urlConfig } from '../../config';
+import React, { useState,useEffect } from 'react';
+import {urlConfig} from '../../config';
 import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
 
-export default function Navbar() {
-    const { isLoggedIn, setIsLoggedIn, userName, setUserName } = useAppContext();
-
+function LoginPage() {
     const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+
     useEffect(() => {
-        const authTokenFromSession = sessionStorage.getItem('auth-token');
-        const nameFromSession = sessionStorage.getItem('name');
-        if (authTokenFromSession) {
-            if (isLoggedIn && nameFromSession) {
-                setUserName(nameFromSession);
-            } else {
-                sessionStorage.removeItem('auth-token');
-                sessionStorage.removeItem('name');
-                sessionStorage.removeItem('email');
-                setIsLoggedIn(false);
-            }
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
         }
-    }, [isLoggedIn, setIsLoggedIn, setUserName])
-    const handleLogout = () => {
-        sessionStorage.removeItem('auth-token');
-        sessionStorage.removeItem('name');
-        sessionStorage.removeItem('email');
-        setIsLoggedIn(false);
-        navigate(`/app`);
+      }, [navigate])
 
-    }
-    const profileSecton = () => {
-        navigate(`/app/profile`);
-    }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
+
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+          setIsLoggedIn(true);
+          navigate('/app');
+        } else {
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+
+      }
+
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar_container'>
-                <a className="navbar-brand" href={`${urlConfig.backendUrl}/app`}>GiftLink</a>
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6 col-lg-4">
+                    <div className="login-card p-4 border rounded">
+                        <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input
+                                id="email"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
+                            />
 
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/app">Gifts</Link> {/* Updated Link */}
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/app/search">Search</Link>
-                        </li>
-                        <ul className="navbar-nav ml-auto">
-                            {isLoggedIn ? (
-                                <>
-                                    <li className="nav-item"> <span className="nav-link" style={{ color: "black", cursor: "pointer" }} onClick={profileSecton}>Welcome, {userName}</span> </li>
-                                    <li className="nav-item"><button className="nav-link login-btn" onClick={handleLogout}>Logout</button></li>
-                                </>
-                            ) : (
-                                <>
-                                    <li className="nav-item">
-
-                                        <Link className="nav-link login-btn" to="/app/login">Login</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link register-btn" to="/app/register">Register</Link>
-                                    </li>
-                                </>
-                            )
-
-                            }
-                        </ul>
-                    </ul>
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+                        </div>
+                        <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
+                        <p className="mt-4 text-center">
+                            New here? <a href="/app/register" className="text-primary">Register Here</a>
+                        </p>
+                    </div>
                 </div>
-            </nav>
-        </>
-    )
+            </div>
+        </div>
+    );
 }
+
+export default LoginPage;
